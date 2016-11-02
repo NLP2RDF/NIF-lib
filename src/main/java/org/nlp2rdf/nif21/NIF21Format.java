416025aad383745600d6fa95756c0682a40b9c43
sandro.coelho <sandro.coelho@afferolab.com.br>
2016-11-02 01:26:59 -0200

    Mapping NIF 21 properties

--- src/main/java/org/nlp2rdf/nif21/NIF21Format.java
+++ src/main/java/org/nlp2rdf/nif21/NIF21Format.java
@@ -6,6 +6,10 @@ public interface NIF21Format extends NIFFormat {
 
     String XML_PREFIX = "http://www.w3.org/2001/XMLSchema#";
 
+    String XML_STRING = XML_PREFIX.concat("string");
+
+    String XML_NON_NEGATIVE_INTEGER = XML_PREFIX.concat("nonNegativeInteger");
+
     String RDF_PREFIX = "http://www.w3.org/2005/11/its/rdf#";
 
     String RDF_PROPERTY_CLASS_REF = RDF_PREFIX.concat("taClassRef");
@@ -15,6 +19,10 @@ public interface NIF21Format extends NIFFormat {
     String RDF_PROPERTY_IDENTREF = RDF_PREFIX.concat("taIdentRef");
 
     String RDF_PROPERTY_ANNOTATOR = RDF_PREFIX.concat("taAnnotatorsRef");
+
+    String RDF_SYNTAX_PREFIX = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+
+    String RDF_SYNTAX_TYPE = RDF_SYNTAX_PREFIX.concat("type");
 
     String NIF_21 = "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core/2.1";
 
@@ -32,6 +40,8 @@ public interface NIF21Format extends NIFFormat {
 
     String NIF_PROPERTY_PHRASE = NIF_CORE_PREFIX.concat("Phrase");
 
+    String NIF_PROPERTY_WORD = NIF_CORE_PREFIX.concat("Word");
+
     String NIF_PROPERTY_ANCHOROF = NIF_CORE_PREFIX.concat("anchorOf");
 
     String NIF_PROPERTY_BEGININDEX = NIF_CORE_PREFIX.concat("beginIndex");
@@ -47,6 +57,24 @@ public interface NIF21Format extends NIFFormat {
     String NIF_PROPERTY_ENTITY_OCCURRENCE = NIF_CORE_PREFIX.concat("EntityOccurrence");
 
     String NIF_PROPERTY_HAS_CONTEXT = NIF_CORE_PREFIX.concat("hasContext");
+
+    String NIF_PROPERTY_PARAGRAPH = NIF_CORE_PREFIX.concat("Paragraph");
+
+    String NIF_PROPERTY_HAS_PARAGRAPH = NIF_CORE_PREFIX.concat("hasParagraph");
+
+    String NIF_PROPERTY_FIRST_PARAGRAPH = NIF_CORE_PREFIX.concat("firstParagraph");
+
+    String NIF_PROPERTY_LAST_PARAGRAPH = NIF_CORE_PREFIX.concat("lastParagraph");
+
+    String NIF_PROPERTY_SECTION = NIF_CORE_PREFIX.concat("Section");
+
+    String NIF_PROPERTY_SUPER_STRING = NIF_CORE_PREFIX.concat("superString");
+
+    String NIF_PROPERTY_PRED_LANG = NIF_CORE_PREFIX.concat("predLang");
+
+    String NIF_PROPERTY_SOURCE_URL = NIF_CORE_PREFIX.concat("sourceUrl");
+
+    String NIF_PROPERTY_HAS_SECTION = NIF_CORE_PREFIX.concat("hasSection");
 
     String CONTEXT_FORMAT = "%s#offset_%d_%d";
 
